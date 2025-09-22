a4a9893f3be59273ce9937b04ea872454f67986d
Michał Krawczyński <michal.krawczynski@orcatech.pl>
2025-09-22 15:25:37 +0200

    app.html stworzenie i wystylizowanie headera oraz footera

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {Header} from './components/header/header';
+import {Footer} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
